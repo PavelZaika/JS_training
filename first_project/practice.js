4927c3da32fd87058ce5008c48ecccfb2f746275
Pavel <zpk.loki@gmail.com>
2021-01-05 15:29:46 +0300

    lesson 15 Practice Updated

--- first_project/practice.js
+++ first_project/practice.js
@@ -11,20 +11,30 @@ const personalMovieDB = {
   aboutLastFilms() {
     for (let i = 0; i < 2; i++) {
       const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+      const ratingofLastMovie = +prompt(
+        'На сколько Вы его оцените от 0 до 10?',
+        0
+      );
 
-      if (lastMovie == '' || lastMovie.length > 50) {
-        alert('Название фильма не должно быть пустым или больше 50 символов!');
-        personalMovieDB.aboutLastFilms();
-      } else {
-        const ratingofLastMovie = prompt('На сколько Вы его оцените?', 0);
+      if (
+        lastMovie != null &&
+        lastMovie != '' &&
+        lastMovie.length < 50 &&
+        ratingofLastMovie != null &&
+        ratingofLastMovie <= 10 &&
+        typeof ratingofLastMovie == 'number'
+      ) {
         personalMovieDB.movies[lastMovie] = `${ratingofLastMovie}`;
+      } else {
+        alert('Не верный ввод!');
+        i--;
       }
     }
   },
   checkCount() {
     if (personalMovieDB.count < 10) {
       alert('Просмотрено мало фильмов!');
-    } else if (personalMovieDB.count < 30) {
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
       alert('Вы классический зритель!');
     } else if (personalMovieDB.count >= 30) {
       alert('Вы Киноман!');

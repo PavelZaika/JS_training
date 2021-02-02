6b46c10cddab762c212f6b599fe593a194bbfbac
Pavel <zpk.loki@gmail.com>
2021-02-02 17:35:53 +0300

    lesson 30 Home work Done

--- second_project/js/script.js
+++ second_project/js/script.js
@@ -15,12 +15,44 @@
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    'Логан',
+    'Лига справедливости',
+    'Ла-ла лэнд',
+    'Одержимость',
+    'Скотт Пилигрим против...',
+    'Аквадискотека',
+  ],
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    remAdv();
+    chngGenre();
+    chngBgr();
+    chngFlms();
+  }, 2000);
+});
+
+const remAdv = () => {
+  const adv = document.querySelector('.promo__adv');
+  adv.remove();
+};
+
+const chngGenre = () => {
+  const genre = document.querySelector('.promo__genre');
+  genre.textContent = 'Драма';
+};
+
+const chngBgr = () => {
+  const chngBgr = document.querySelector('.promo__bg');
+  chngBgr.style.backgroundImage = 'url("img/bg.jpg")';
+};
+
+const chngFlms = () => {
+  const listOfFilms = document.querySelectorAll('.promo__interactive-item');
+  movieDB.movies.sort();
+  listOfFilms.forEach((element, key) => {
+    element.textContent = `${key + 1}.${movieDB.movies[key]}`;
+  });
+};

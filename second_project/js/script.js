6e743a6c6936f0c6fa1344242b5b3318dbe9da09
PavelZaika <zpk.loki@gmail.com>
2021-02-03 18:08:16 +0300

    33 lesson Practice Done Updated

--- second_project/js/script.js
+++ second_project/js/script.js
@@ -1,20 +1,3 @@
-/* Задания на урок:
-
-1. Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" -
-   новый фильм добавляется в список. Страница не должна перезагружаться.
-   Новый фильм должен добавляться в movieDB.movies.
-   Для получения доступа к значению input - обращаемся к нему как input.value;
-   P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2. Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3. При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4. Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
-   "Добавляем любимый фильм"
-
-5. Фильмы должны быть отсортированы по алфавиту \*/
-
 'use strict';
 
 document.addEventListener('DOMContentLoaded', e => {
@@ -26,7 +9,6 @@ document.addEventListener('DOMContentLoaded', e => {
   const genre = document.querySelector('.promo__genre');
   const chngBack = document.querySelector('.promo__bg');
   const listOfFilms = document.querySelector('.promo__interactive-list');
-  const btn = document.querySelector('button');
   const addForm = document.querySelector('form.add');
   const addInput = addForm.querySelector('.adding__input');
   const checkbox = addForm.querySelector('[type="checkbox"]');
@@ -60,12 +42,6 @@ document.addEventListener('DOMContentLoaded', e => {
   });
 
   const remAdv = () => {
-    //Первый вариант
-    //   const adv = document.querySelector('.promo__adv');
-    //   adv.remove();
-
-    // Второй вариант. Визуально получается лучше в результате
-
     setTimeout(() => {
       adv.forEach(item => {
         item.remove();
@@ -108,5 +84,6 @@ document.addEventListener('DOMContentLoaded', e => {
 
   remAdv(adv);
   chngBgr();
+  chngGenre();
   chngFlms(movieDB.movies, listOfFilms);
 });

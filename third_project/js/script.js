e75ba438ca2bda79191ba7b475931c7ab49430e5
PavelZaika <zpk.loki@gmail.com>
2021-08-13 13:44:17 +0300

    RegExp

--- third_project/js/script.js
+++ third_project/js/script.js
@@ -370,11 +370,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
+
   next.addEventListener('click', () => {
-    if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.replace(/\D/g, '');
+      offset += deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -392,9 +396,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.replace(/\D/g, '') * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= +width.replace(/\D/g, '');
+      offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -416,7 +420,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const slideTo = e.target.getAttribute('data-slide-to');
 
         slideIndex = slideTo;
-        offset = +width.replace(/\D/g, '') * (slideTo - 1);
+        offset = deleteNotDigits(width) * (slideTo - 1);
         slidesField.style.transform = `translateX(-${offset}px)`;
 
         addZeroToSmallIndex();
